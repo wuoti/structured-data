687e483df28896b079d762df0150f5e190bdf02b
Kalle Wuoti <kalle.wuoti@gofore.com>
2014-11-03 09:06:33 +0200

    First tasks done

--- src/structured_data.clj
+++ src/structured_data.clj
@@ -1,16 +1,18 @@
 (ns structured-data)
 
 (defn do-a-thing [x]
-  :-)
+  (let [double (+ x x)]
+  (Math/pow double double)))
 
 (defn spiff [v]
-  :-)
+  (if (< (count v) 3) nil (+ (get v 0) (get v 2))))
 
 (defn cutify [v]
-  :-)
+  (conj v "<3"))
 
-(defn spiff-destructuring [v]
-  :-)
+(defn spiff-destructuring [[f _ t]]
+    (+ f t)
+    )
 
 (defn point [x y]
   [x y])
@@ -18,32 +20,42 @@
 (defn rectangle [bottom-left top-right]
   [bottom-left top-right])
 
-(defn width [rectangle]
-  :-)
+(defn abs [n] (max n (- n)))
 
-(defn height [rectangle]
-  :-)
+(defn width [[[x1 _] [x2 _]]]
+  (abs (- x1 x2))
+  )
+
+(defn height [[[_ y1] [_ y2]]]
+  (abs (- y1 y2))
+  )
 
 (defn square? [rectangle]
-  :-)
+  (= (height rectangle) (width rectangle))
+  )
 
 (defn area [rectangle]
-  :-)
+  (* (height rectangle) (width rectangle)))
 
-(defn contains-point? [rectangle point]
-  :-)
+(defn contains-point? [[[x1 y1] [x2 y2]] [px py]]
+  (and (<= x1 px x2) (<= y1 py y2))
+  )
 
-(defn contains-rectangle? [outer inner]
-  :-)
+(defn contains-rectangle? [outer [bottom-left top-right]]
+  (and (contains-point? outer bottom-left) (contains-point? outer top-right))
+  )
 
 (defn title-length [book]
-  :-)
+  (count (:title book))
+  )
 
 (defn author-count [book]
-  :-)
+  (count (:authors book))
+  )
 
 (defn multiple-authors? [book]
-  :-)
+  (> (author-count book) 1)
+  )
 
 (defn add-author [book new-author]
   :-)
